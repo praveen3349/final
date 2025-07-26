fbeb585f3aa78709e1d6cc4082aec2f09c6ee0a2
praveen <ppraveen2150@gmail.com>
2025-07-26 10:32:16 +0530

    flake8 corrections commit

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask
+
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return "Hello, CI/CD!"
 
+
 @app.route('/status')
 def status():
     return {"status": "running"}, 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
